27832c7df1fdc29678865f019eba11d1f91e24f5
SliverGin <85085877+SliverGin@users.noreply.github.com>
2021-09-20 22:05:43 +0700

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,1 +1,14 @@
 
+if (message.content.startsWith(config.prefix + "avatar")) {
+      if (!message.mentions.users.size) {
+        const avatarAuthor = new Discord.RichEmbed()
+      .setColor(0x333333)
+      .setAuthor(message.author.username)
+      .setImage(message.author.avatarURL)
+        message.channel.send(avatarAuthor);
+        let mention = message.mentions.members.first();
+        const avatarMention = new Discord.RichEmbed()
+        .setColor(0x333333)
+        .setAuthor(mention.user.username)
+        .setImage(mention.user.avatarURL)
+        message.channel.send(avatarMention);
